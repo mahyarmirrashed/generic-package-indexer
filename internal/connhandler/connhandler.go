f9aebb90f7de3dc19972adb02388dc5495a6fb3c
Mahyar Mirrashed <mah.mirr@gmail.com>
2025-07-26 00:56:24 -0500

    feat: parse incoming messages

--- internal/connhandler/connhandler.go
+++ internal/connhandler/connhandler.go
@@ -5,6 +5,8 @@ import (
 	"fmt"
 	"log"
 	"net"
+
+	"example.com/generic-package-indexer/internal/parser"
 )
 
 func HandleConnection(conn net.Conn) {
@@ -17,8 +19,17 @@ func HandleConnection(conn net.Conn) {
 		line := scanner.Text()
 		log.Printf("[client %s] Received: %s", remoteAddr, line)
 
+		req, err := parser.Parse(line)
+		if err != nil {
+			log.Printf("[client %s] Failed to parse message: %v", remoteAddr, err)
+			return
+		}
+
+		// Log parsed message for now...
+		log.Printf("[client %s] Parsed message: %s %s %s", remoteAddr, req.Command, req.Package, req.Dependencies)
+
 		// Respond with ok to every message for now...
-		_, err := fmt.Fprint(conn, "OK\n")
+		_, err = fmt.Fprint(conn, "OK\n")
 		if err != nil {
 			log.Printf("[client %s] Failed to send response: %v", remoteAddr, err)
 			return
