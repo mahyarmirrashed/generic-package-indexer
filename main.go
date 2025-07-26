ec03324826dde51e13645362ff7339030ee6bed8
Mahyar Mirrashed <mah.mirr@gmail.com>
2025-07-26 00:56:24 -0500

    feat: add client to handle multiple connections
    For now, just respond back with OK.

--- main.go
+++ main.go
@@ -1,7 +1,48 @@
 package main
 
-import "fmt"
+import (
+	"bufio"
+	"fmt"
+	"log"
+	"net"
+)
 
 func main() {
-	fmt.Println("Hello")
+	addr := ":8080"
+	ln, err := net.Listen("tcp", addr)
+	if err != nil {
+		log.Fatalf("Failed to listen on %s: %v", addr, err)
+	}
+	log.Printf("Listening on %s", addr)
+
+	for {
+		conn, err := ln.Accept()
+		if err != nil {
+			log.Printf("Failed to accept connection: %v", err)
+			continue
+		}
+
+		go handleConnection(conn)
+	}
+}
+
+func handleConnection(conn net.Conn) {
+	defer conn.Close()
+	scanner := bufio.NewScanner(conn)
+
+	for scanner.Scan() {
+		line := scanner.Text()
+		log.Printf("Received: %s", line)
+
+		// Respond with ok to every message for now...
+		_, err := fmt.Fprint(conn, "OK\n")
+		if err != nil {
+			log.Printf("Failed to send response: %v", err)
+			return
+		}
+	}
+
+	if err := scanner.Err(); err != nil {
+		log.Printf("Connection error: %v", err)
+	}
 }
