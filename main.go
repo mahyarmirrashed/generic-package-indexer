d5f20563fcf4dcb6a4a1043a31f6a7369c63eb8a
Mahyar Mirrashed <mah.mirr@gmail.com>
2025-07-26 00:56:24 -0500

    chore: update logging messages

--- main.go
+++ main.go
@@ -12,16 +12,16 @@ func main() {
 	addr := ":8080"
 	ln, err := net.Listen("tcp", addr)
 	if err != nil {
-		log.Fatalf("Failed to listen on %s: %v", addr, err)
+		log.Fatalf("[server] Failed to listen on %s: %v", addr, err)
 	}
-	log.Printf("Listening on %s", addr)
+	log.Printf("[server] Listening on %s", addr)
 
 	idx := indexer.New()
 
 	for {
 		conn, err := ln.Accept()
 		if err != nil {
-			log.Printf("Failed to accept connection: %v", err)
+			log.Printf("[server] Failed to accept connection: %v", err)
 			continue
 		}
 
